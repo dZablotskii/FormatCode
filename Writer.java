eecd3b08bc4d436ed083a7d7df12d9407561c35d
dZablotskii <denisvirus@yandex.ru>
2014-10-18 14:48:18 +0700

    Write of data

---
+++ Writer.java
@@ -0,0 +1,21 @@
+/**
+*OutPut of Data.
+*/
+import java.io.*;
+class Writer{
+	private static BufferedWriter bw;
+	public static void ini()throws IOException{
+		bw= new BufferedWriter(new FileWriter("True.txt"));
+	}
+	public void writeFile(String s)throws IOException{
+		try{
+			bw.write(s);
+		}
+		catch(Exception e){
+
+		}
+	}
+	public void closeWrite()throws IOException{
+		bw.close();
+	}
+}
